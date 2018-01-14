1748f3cf6a8fa3b9003985cbd4c273f7c0c4bf9d
Dudu Cohen <duduc54@gmail.com>
2018-01-14 09:35:29 +0200

    no message

--- app/src/main/java/com/cdh/instacast/utils/Consts.java
+++ app/src/main/java/com/cdh/instacast/utils/Consts.java
@@ -7,7 +7,7 @@ package com.cdh.instacast.utils;
 public class Consts {
 
     public static final String BASE_URL = "https://api.instagram.com/v1/";
-    public static final String ACCESS_TOKEN = "Put Your Access Token Here";
+    public static final String ACCESS_TOKEN = "732825190.ad9b631.a25089355471484fade8836d7710c9aa";//"Put Your Access Token Here";
     public static final String RESULTS_COUNT = "10";
     public static final int PERMISSION_ACCESS_FINE_LOCATION = 999;
     public static final int DEFAULT_DISTANCE = 500;  // start distance
